0aad42d2e08f0409a6903b8dc073d387301f48c2
Dim <dim@dim.com>
2018-10-09 15:56:54 +0600

    Initial commit

---
+++ user_data/user_data.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -xe
+
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum -y install mc docker git
+usermod -aG docker ec2-user
+systemctl enable docker
+chkconfig docker on
+systemctl start docker
+
+curl -L "https://github.com/docker/compose/releases/download/1.23.0-rc2/docker-compose-Linux-x86_64" -o /usr/bin/docker-compose
+chmod +x /usr/bin/docker-compose
+
+git clone https://github.com/bigstinky86/aws-module4.git /home/module4
