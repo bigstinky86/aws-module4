9f7d1ef8a763381486e3a8d5fbe1c5681bdbcdb9
Dim <dim@dim.com>
2018-10-11 16:14:12 +0600

    Changes in startup script

--- dbserver/dbserver_startup.sh
+++ dbserver/dbserver_startup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
+mysqld_safe
+
 mysql --protocol=socket -uroot -p$MYSQL_ROOT_PASSWORD <<EOSQL
 CREATE DATABASE moduledb;
 GRANT ALL ON moduledb.* TO '$USER_LOGIN'@'%' IDENTIFIED BY '$USER_PASS';
 EOSQL
-
-mysqld_safe
