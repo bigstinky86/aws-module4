28123bdf4679aa9bb4e57e6e07813ba8412cf99b
Dim <dim@dim.com>
2018-10-12 13:41:47 +0600

    Added docker pull command

--- user_data/user_data.sh
+++ user_data/user_data.sh
@@ -10,4 +10,6 @@ systemctl start docker
 curl -L "https://github.com/docker/compose/releases/download/1.23.0-rc2/docker-compose-Linux-x86_64" -o /usr/bin/docker-compose
 chmod +x /usr/bin/docker-compose
 
+docker pull amazonlinux
+
 git clone https://github.com/bigstinky86/aws-module4.git /home/module4
