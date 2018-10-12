6c784d6a5a9ec79762bf85b83fd4e52652a26d2d
Dim <dim@dim.com>
2018-10-12 12:09:56 +0600

    Changed index.php file

--- backend/index.php
+++ backend/index.php
@@ -1,6 +1,19 @@
 <?php
 
-echo gethostname();
+echo "Hostname: " . gethostname() . "<br />";
 
-echo "It works";
+$dbname = getenv("DB_NAME");
+$dbhost = getenv("DB_HOST");
+$dbuser = getenv("DB_USER");
+$dbpass = getenv("DB_PASS");
 
+echo "Connection status: ";
+
+try {
+    $dbh = new PDO("mysql:host=" . $dbhost . ";dbname=" . $dbname, $dbuser, $dbpass, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    $dbh=null;
+    echo "Connected to database!<br />";
+}
+catch(PDOException $e){
+    echo $e->getMessage() . "<br />";
+}
